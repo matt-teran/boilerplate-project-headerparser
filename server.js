039cc5334999dac5e585418e910fc9156de8556b
Matthew Teran <matt@Matthews-Air.lan1>
2020-04-14 22:57:36 -0500

    Added api endpoint to output users IP address, language and agent

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req,res)=>{
+  res.json({ipaddress: req.connection.localAddress, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
